a2d62bfc84147cc66b12c0120181480d414f37c3
ext_tangwen <18300613164@163.com>
2025-09-02 04:17:02 +0800

    feat: init

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,6 +1,6 @@
 import { Mastra } from "@mastra/core/mastra";
 import { PinoLogger } from "@mastra/loggers";
-import { LibSQLStore } from "@mastra/libsql";
+// import { LibSQLStore } from "@mastra/libsql";
 import { weatherWorkflow } from "./workflows/weather-workflow";
 import { weatherAgent } from "./agents/weather-agent";
 import { CloudflareDeployer } from "@mastra/deployer-cloudflare";
@@ -8,10 +8,10 @@ import { CloudflareDeployer } from "@mastra/deployer-cloudflare";
 export const mastra = new Mastra({
   workflows: { weatherWorkflow },
   agents: { weatherAgent },
-  storage: new LibSQLStore({
-    // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
-    url: ":memory:",
-  }),
+  // storage: new LibSQLStore({
+  //   // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
+  //   url: ":memory:",
+  // }),
   logger: new PinoLogger({
     name: "Mastra",
     level: "info",
