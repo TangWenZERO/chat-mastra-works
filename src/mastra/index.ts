8e8db35e7ff3ae6e730de3a9aaf512c820b940cb
ext_tangwen <18300613164@163.com>
2025-09-02 03:46:25 +0800

    feat: init

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,30 +1,19 @@
-import { Mastra } from "@mastra/core/mastra";
-import { PinoLogger } from "@mastra/loggers";
-// import { LibSQLStore } from "@mastra/libsql";
-import { weatherWorkflow } from "./workflows/weather-workflow";
-import { weatherAgent } from "./agents/weather-agent";
-import { CloudflareDeployer } from "@mastra/deployer-cloudflare";
-// 检查运行环境
-const isWorkerEnv =
-  typeof navigator !== "undefined" &&
-  navigator.userAgent.includes("Cloudflare-Workers");
 
-const dbConfig = isWorkerEnv
-  ? { url: "libsql://:memory:" } // Worker 环境使用内存数据库
-  : { url: "file:./local.db" }; // Node 环境可以使用文件数据库
+import { Mastra } from '@mastra/core/mastra';
+import { PinoLogger } from '@mastra/loggers';
+import { LibSQLStore } from '@mastra/libsql';
+import { weatherWorkflow } from './workflows/weather-workflow';
+import { weatherAgent } from './agents/weather-agent';
 
 export const mastra = new Mastra({
   workflows: { weatherWorkflow },
   agents: { weatherAgent },
-  // storage: new LibSQLStore(dbConfig),
-  logger: new PinoLogger({
-    name: "Mastra",
-    level: "info",
+  storage: new LibSQLStore({
+    // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
+    url: ":memory:",
   }),
-  deployer: new CloudflareDeployer({
-    projectName: "hello-mastra",
-    env: {
-      NODE_ENV: "production",
-    },
+  logger: new PinoLogger({
+    name: 'Mastra',
+    level: 'info',
   }),
 });
